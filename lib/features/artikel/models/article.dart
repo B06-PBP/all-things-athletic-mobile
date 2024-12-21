6a1ed2d99e97fdea603629babf1ce5d055d3f0f8
shamimasen <shalyalionita@gmail.com>
2024-12-21 22:26:49 +0700

    updated artikel

--- lib/features/artikel/models/article.dart
+++ lib/features/artikel/models/article.dart
@@ -1,35 +1,35 @@
 class Article {
-    String model;
-    int pk;
-    Fields fields;
+  String model;
+  int pk;
+  Fields fields;
 
-    Article({
-        required this.model,
-        required this.pk,
-        required this.fields,
-    });
+  Article({
+    required this.model,
+    required this.pk,
+    required this.fields,
+  });
 
-    factory Article.fromJson(Map<String, dynamic> json) => Article(
+  factory Article.fromJson(Map<String, dynamic> json) => Article(
         model: json["model"],
         pk: json["pk"],
         fields: Fields.fromJson(json["fields"]),
-    );
+      );
 }
 
 class Fields {
-    String title;
-    String imgUrl;
-    String fullDescription;
+  String title;
+  String imgUrl;
+  String fullDescription;
 
-    Fields({
-        required this.title,
-        required this.imgUrl,
-        required this.fullDescription,
-    });
+  Fields({
+    required this.title,
+    required this.imgUrl,
+    required this.fullDescription,
+  });
 
-    factory Fields.fromJson(Map<String, dynamic> json) => Fields(
+  factory Fields.fromJson(Map<String, dynamic> json) => Fields(
         title: json["title"],
         imgUrl: json["image_url"],
         fullDescription: json["full_description"],
-    );
+      );
 }
